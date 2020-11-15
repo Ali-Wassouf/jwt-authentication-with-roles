39ac295cfb3221692b59983d7ae770bfbda74548
Ali Wassouf <ali.wassouf@jimdo.com>
2020-11-15 20:18:46 +0100

    test(JwtProvider): Added more unit test coverage

--- src/test/kotlin/com/tyche/auth/authentication/security/JwtProviderTest.kt
+++ src/test/kotlin/com/tyche/auth/authentication/security/JwtProviderTest.kt
@@ -1,16 +1,22 @@
 package com.tyche.auth.authentication.security
 
 import com.tyche.auth.authentication.domainobject.Role
+import com.tyche.auth.authentication.exception.TokenValidationException
 import com.tyche.auth.authentication.service.CustomUserDetailsService
 import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
 import io.mockk.every
 import io.mockk.mockk
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.junit.MockitoJUnitRunner
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.User
+import org.testcontainers.shaded.org.apache.commons.lang.time.DateUtils
+import java.util.*
 import javax.servlet.http.HttpServletRequest
+import kotlin.collections.LinkedHashMap
 
 @RunWith(MockitoJUnitRunner::class)
 internal class JwtProviderTest {
@@ -51,25 +57,37 @@ internal class JwtProviderTest {
     }
 
     @Test
-    fun `when getting authentication from a token, return correct user`(){
+    fun `when getting authentication from a token, return correct user`() {
         every { customUserDetailsService.loadUserByUsername("aliwassouf") }.returns(User("aliwassouf", "password", emptyList()))
         val authentication = jwtProvider.getAuthentication(testToken)
-        assertEquals("aliwassouf",(authentication.principal as User).username)
+        assertEquals("aliwassouf", (authentication.principal as User).username)
     }
 
     @Test
-    fun `resolving a request returns a token`(){
+    fun `resolving a request returns a token`() {
         every { httpServletRequest.getHeader("Authorization") }.returns("${SecurityConstants.TOKEN_PREFIX}$testToken")
-        val returnedToken  = jwtProvider.resolveToken(httpServletRequest)
+        val returnedToken = jwtProvider.resolveToken(httpServletRequest)
         assertEquals(testToken, returnedToken)
     }
 
     @Test
-    fun `resolving request returns null`(){
+    fun `resolving request returns null`() {
         every { httpServletRequest.getHeader("Authorization") }.returns(testToken)
-        val returnedToken  = jwtProvider.resolveToken(httpServletRequest)
+        val returnedToken = jwtProvider.resolveToken(httpServletRequest)
         assertNull(returnedToken)
     }
+
+
+    @Test
+    fun `when validating a valid token return true`() {
+        assertTrue(jwtProvider.validateToken(createValidToken()))
+    }
+
+    @Test
+    fun `when validating a non-valid token throws an excption`() {
+        assertThrows(TokenValidationException::class.java) {jwtProvider.validateToken(createExpiredToken())}
+    }
+
 
     private fun getSubjectFromToken(token: String): String {
         return Jwts.parser().setSigningKey(SecurityConstants.SECRET).parseClaimsJws(token).body.subject
@@ -78,5 +96,26 @@ internal class JwtProviderTest {
     private fun getAuthFromToken(token: String): Any? {
         return Jwts.parser().setSigningKey(SecurityConstants.SECRET).parseClaimsJws(token).body["auth"]
 
+    }
+
+    private fun createValidToken(): String {
+        return createToken(Date())
+
+    }
+
+    private fun createExpiredToken(): String {
+        return createToken(DateUtils.addYears(Date(), -2))
+    }
+
+    private fun createToken(date: Date): String {
+        val claims = Jwts.claims().setSubject("username")
+        claims["auth"] = SimpleGrantedAuthority("USER")
+
+        val validity = Date(date.time + SecurityConstants.EXPIRATION_TIME)
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(date)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, SecurityConstants.SECRET).compact()
     }
 }
